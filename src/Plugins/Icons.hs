7965f0ef3486dc7ce97e9cf254bc14e532f98c2c
Edward O'Callaghan <victoredwardocallaghan@gmail.com>
2013-01-21 17:29:22 +1100

    clean up.

--- src/Plugins/Icons.hs
+++ src/Plugins/Icons.hs
@@ -17,30 +17,11 @@ module Plugins.Icons where
 import Plugins
 import XGraphic
 import Graphics.X11
---import Graphics.X11.Xlib
---import Graphics.X11.Types
---import Foreign.C.Types
---import Graphics.X11.Xlib.Misc
---import Graphics.X11.Xlib.Context
---import Graphics.X11.Xlib.Extras
---import Data.List
---import Data.Bits
---import Control.Monad
---import System.FilePath
---import XUtil
---import Control.Concurrent (threadDelay)
+import Control.Concurrent (threadDelay)
 
 data Icons = Icons
     deriving (Read, Show)
 
-drawInWin :: Display -> Window -> String -> IO ()
-drawInWin dpy win str = do
- gc <- createGC dpy win
- --p <- createPixmap dpy win 200 100 (defaultDepthOfScreen (defaultScreenOfDisplay dpy))
- (bitmap_width, bitmap_height, p, _, _) <- readBitmapFile dpy win str
- copyPlane dpy p win gc 0 0 bitmap_width bitmap_height 0 0 1
- freeGC dpy gc
- freePixmap dpy p
 
 instance Exec Icons where
     alias Icons = "icons"
@@ -48,8 +29,17 @@ instance Exec Icons where
     run Icons = do
     d <- openDisplay ""
     root <- rootWindow d (defaultScreen d)
-    drawInWin d root "cat.xbm"
-    sync d True
+    drawInWin d root "cat.xbm" -- FIXME: hardcoded icon
+    sync d False
+    threadDelay (1 * 1000000)
     closeDisplay d
     return "<fc=red>Hello World!!</fc>"
---        return $ unwords $ map snd ls
+
+
+drawInWin :: Display -> Window -> String -> IO ()
+drawInWin dpy win str = do
+ gc <- createGC dpy win
+ (bitmap_width, bitmap_height, p, _, _) <- readBitmapFile dpy win str
+ copyPlane dpy p win gc 0 0 bitmap_width bitmap_height 0 0 1
+ freeGC dpy gc
+ freePixmap dpy p
