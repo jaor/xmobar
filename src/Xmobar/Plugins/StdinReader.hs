jaor/xmobar

commit b7a3d674581720bfb63bf73cae8368ebbad81004

Avoid busy looping by not catching all exceptions
This specifically avoids situation described in this issue
https://github.com/jaor/xmobar/issues/438 where the handle was
throwing the IOException continously in a loop:

<stdin>: hGetLine: invalid argument (invalid byte sequence)

It happened because my system's environment was right, but the proper
behaviour hear would be to let it to throw the exception rather than
leading to a busy loop.

I did some git blame to find out that this commit introduced the
behaviour:
https://github.com/jaor/xmobar/commit/fc24dc1874dcf7c9e66e21502a58b40cbe627c85
but there was no reason mentioned in the commit for trying to capture
all exceptions.

src/Xmobar/Plugins/StdinReader.hs
@@ -31,7 +31,7 @@ data StdinReader = StdinReader | UnsafeStdinReader
 
 instance Exec StdinReader where
   start stdinReader cb = do
-    s <- handle (\(SomeException e) -> do hPrint stderr e; return "") getLine
+    s <- getLine
     cb $ escape stdinReader s
     eof <- isEOF
     if eof

commit 89d71dcc382a77b80b60efd5bf1c8dc81b9d2e85

StdinReader: Remove throttling on exception
Introducing the throttling unfortunately has a negative side-effect: it
delays all stdin processing, including EOF detection, which can cause
confusion the previous commit tries to fix.

The only benefit of the throttling is to prevent 100% CPU usage when a
lot of garbage is provided on xmobar stdin. We still don't know where
that garbage comes from in https://github.com/jaor/xmobar/issues/438, or
rather why there's more than a handful of lines of such garbage. @psibi
has since fixed his setup to not produce that garbage, and no one else
ever reported such a problem, so it's probably safe to ignore it for
now.

Should anyone ever encounter that again, feel free to ping me, even in
the middle of the night, to help debug this.

Fixes: 7759df11f746 ("StdinReader: Improve exception handling")
Fixes: b7a3d6745817 ("Avoid busy looping by not catching all exceptions")

src/Xmobar/Plugins/StdinReader.hs
@@ -27,7 +27,6 @@ import System.IO
 import System.IO.Error (isEOFError)
 import Xmobar.Run.Exec
 import Xmobar.X11.Actions (stripActions)
-import Control.Concurrent (threadDelay)
 import Control.Exception
 import Control.Monad (forever)
 
@@ -45,15 +44,11 @@ instance Exec StdinReader where
       -- there'd be a pileup of xmobars
       handler (fromException -> Just e) | isEOFError e = exitImmediately ExitSuccess
       -- any other exception, like "invalid argument (invalid byte sequence)",
-      -- is logged to both stderr and the bar itself, throttled to avoid
-      -- excessive CPU usage whenever someone pipes garbage into xmobar, and
-      -- then discarded without terminating, so a single charset error doesn't
-      -- break the entire xmobar
+      -- is logged to both stderr and the bar itself
       handler e = do
         let errorMessage = "xmobar: Received exception " <> show e
         hPutStrLn stderr errorMessage
         cb $ stripActions errorMessage
-        threadDelay 1000000
 
 escape :: StdinReader -> String -> String
 escape StdinReader = stripActions